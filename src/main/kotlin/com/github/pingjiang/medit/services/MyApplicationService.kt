a8e8da04cb93c08986155de417a10c6e8b1e3a6c
GitHub Action <action@github.com>
2020-11-09 11:35:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/pingjiang/medit/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pingjiang/medit/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pingjiang.medit.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pingjiang.medit.MyBundle
 
 class MyApplicationService {
 
